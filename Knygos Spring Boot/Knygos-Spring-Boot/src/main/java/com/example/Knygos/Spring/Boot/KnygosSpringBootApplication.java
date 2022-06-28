3f02604251d129421720e7bc66e06928515bedd0
Juozapas Liubauskas <juozapas.liubauskas@gmail.com>
2022-06-28 16:58:54 +0300

    added spring boot

---
+++ Knygos Spring Boot/Knygos-Spring-Boot/src/main/java/com/example/Knygos/Spring/Boot/KnygosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Knygos.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnygosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnygosSpringBootApplication.class, args);
+	}
+
+}
---
+++ Knygos Spring Boot/Knygos-Spring-Boot/src/test/java/com/example/Knygos/Spring/Boot/KnygosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Knygos.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnygosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
